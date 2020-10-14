f5cd92595ed5b8b0b0968fa79ddbe396ad504dfd
Noah K <nwk6661@rit.edu>
2020-10-14 00:55:11 -0400

    Add employee

---
+++ service/src/main/java/com/kennuware/erp/manufacturing/service/model/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.kennuware.erp.manufacturing.service.model.repository;
+
+import com.kennuware.erp.manufacturing.service.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+  Employee findByUsername(String username);
+
+}
--- service/src/main/java/com/kennuware/erp/manufacturing/service/model/repository/QueueRepository.java
+++ service/src/main/java/com/kennuware/erp/manufacturing/service/model/repository/QueueRepository.java
@@ -4,7 +4,7 @@ import com.kennuware.erp.manufacturing.service.model.Queue;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QueueRepository extends JpaRepository<Queue, Long> {
+public interface QueueRepository extends JpaRepository<Queue, String> {
 
   Optional<Queue> findByName(String name);
 
