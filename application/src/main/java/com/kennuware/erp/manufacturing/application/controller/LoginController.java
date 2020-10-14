26f95d2c37a548189630f1c62ead17c4aa6d1ebf
Kyle McCoy <mrkylermccoy@gmail.com>
2020-10-14 14:02:09 -0400

    fixed LoginController

--- application/src/main/java/com/kennuware/erp/manufacturing/application/controller/LoginController.java
+++ application/src/main/java/com/kennuware/erp/manufacturing/application/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.kennuware.erp.manufacturing.application.controller;
 
+import com.kennuware.erp.manufacturing.application.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,16 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class LoginController {
     @RequestMapping(path = "/sign_in")
-    public RedirectView redirectToProcess(@RequestParam String user, @RequestParam String pass, Model model){
+    public Object redirectToProcess(@RequestParam String user, @RequestParam String pass, Model model){
         // INSERT SIGN IN CHECKING HERE!!!
         if (user.isBlank() || pass.isBlank()) {
             model.addAttribute("failure", true);
-            return new RedirectView("");
+            String temp = new String("login");
+            return temp;
         }
+        RestTemplate rt = new RestTemplate();
+        Order[] queue = rt.getForObject("http://localhost:8080/manufacturing/api/queue/?queueName=Main Queue", Order[].class);
+        model.addAttribute("queue", queue);
         return new RedirectView("/process");
     }
 
