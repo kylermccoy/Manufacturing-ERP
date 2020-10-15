a3c81e4d414b364d31acdb402f5569782a5a24cc
Noah K <nwk6661@rit.edu>
2020-10-15 02:05:03 -0400

    Add default user

--- service/src/main/java/com/kennuware/erp/manufacturing/service/LoadDatabase.java
+++ service/src/main/java/com/kennuware/erp/manufacturing/service/LoadDatabase.java
@@ -3,20 +3,28 @@ package com.kennuware.erp.manufacturing.service;
 import com.kennuware.erp.manufacturing.service.controller.QueueController;
 import com.kennuware.erp.manufacturing.service.model.Employee;
 import com.kennuware.erp.manufacturing.service.model.Queue;
+import com.kennuware.erp.manufacturing.service.model.repository.EmployeeRepository;
 import com.kennuware.erp.manufacturing.service.model.repository.QueueRepository;
 import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Slf4j
 public class LoadDatabase {
 
   @Bean
-  CommandLineRunner initDatabase(QueueRepository repository) {
+  CommandLineRunner initDatabase(QueueRepository repository, EmployeeRepository employeeRepository, PasswordEncoder encoder) {
     return args -> {
+      Employee e = new Employee();
+      e.setUsername("admin");
+      e.setPassword(encoder.encode("admin"));
+      if (!employeeRepository.existsByUsername("admin")) {
+        log.info("Preloading " + employeeRepository.save(e));
+      }
       Queue q = new Queue();
       q.setName(QueueController.QUEUE_NAME);
       q.setRunning(false);
