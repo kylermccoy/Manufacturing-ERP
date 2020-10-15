2b4aac251a8f5b7db5393d645c425faeaeea775a
Noah K <nwk6661@rit.edu>
2020-10-15 00:06:47 -0400

    Add deleting order and recall

---
+++ application/src/main/resources/static/js/actions.js
@@ -0,0 +1,17 @@
+$( function() {
+  $("#product-list").find("button").click(function() {
+    let attrId = $(this).attr("id");
+    let orderId = attrId.split("-")[1];
+    let url = "http://localhost:8080/manufacturing/api/requests/" + orderId;
+    sendDeleteRequest(url);
+  })
+} )
+
+function sendDeleteRequest(url) {
+  $.ajax({
+    url: url,
+    type: 'DELETE'
+  })
+  .done(function() {location.reload()})
+  .fail(function() {console.log("fail")});
+}
