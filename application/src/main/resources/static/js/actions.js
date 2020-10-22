32d8b018e6f4ee2adb877ac1ec8ef042106ae096
Kyle McCoy <mrkylermccoy@gmail.com>
2020-10-21 21:02:13 -0400

    spacing

--- application/src/main/resources/static/js/actions.js
+++ application/src/main/resources/static/js/actions.js
@@ -1,34 +1,34 @@
 $( function() {
-  $("#product-list").find("button").click(function() {
-    let attrId = $(this).attr("id");
-    let orderId = attrId.split("-")[1];
-    let url = "http://localhost:8080/manufacturing/api/requests/" + orderId;
-    sendDeleteRequest(url);
-  })
+    $("#product-list").find("button").click(function() {
+        let attrId = $(this).attr("id");
+        let orderId = attrId.split("-")[1];
+        let url = "http://localhost:8080/manufacturing/api/requests/" + orderId;
+        sendDeleteRequest(url);
+    })
 } )
 
 $( function() {
-  $("#controls").find("button").click(function() {
-    let command = $(this).attr("id")
-    let url = "http://localhost:8080/manufacturing/api/queue/" + command;
-    sendProcessControlRequest(url);
-  })
+    $("#controls").find("button").click(function() {
+        let command = $(this).attr("id")
+        let url = "http://localhost:8080/manufacturing/api/queue/" + command;
+        sendProcessControlRequest(url);
+    })
 } )
 
 function sendDeleteRequest(url) {
-  $.ajax({
-    url: url,
-    type: 'DELETE'
-  })
-      .done(function() {location.reload()})
-      .fail(function() {console.log("fail")});
+    $.ajax({
+        url: url,
+        type: 'DELETE'
+    })
+        .done(function() {location.reload()})
+        .fail(function() {console.log("fail")});
 }
 
 function sendProcessControlRequest(url) {
-  $.ajax({
-    url: url,
-    type: 'GET'
-  })
-      .done(function() {location.reload()})
-      .fail(function() {console.log("fail")});
+    $.ajax({
+        url: url,
+        type: 'GET'
+    })
+        .done(function() {location.reload()})
+        .fail(function() {console.log("fail")});
 }
